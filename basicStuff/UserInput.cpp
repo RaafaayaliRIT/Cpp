8c40f36168c46662d9d50ed040ee41001d64f90f
Raafay Ali <rsa1184@g.rit.edu>
2026-02-09 20:52:13 -0500

    idk

--- basicStuff/UserInput.cpp
+++ basicStuff/UserInput.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
-using namespace std;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
 int main(){
     int age;
     cout << "Please enter your age: ";
     cin >> age; // gets the userinput from the user
-    cout << "Your are " << age << " years old!";
+    cout << "Your are " << age << " years old!" << endl;
+
+    // you can use the getline()
+
+    string name;
+    cout << "please enter your name: ";
+    std::getline(cin >> std::ws,name);
+    cout << "Hello, " << name+"!";
+
+    
     return 0;
 }
